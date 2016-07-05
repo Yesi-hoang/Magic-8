d3cf7af1400f7377bff7b47cfb20fdd9d61d9582
Yesi <317447721@qq.com>
2016-07-05 22:48:50 +0800

    Mac Swift Magic 8
    Mac Swift Magic 8

--- Magic-8/ViewController.swift
+++ Magic-8/ViewController.swift
@@ -9,19 +9,66 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet weak var adviceTextField: NSTextField!
 
+    @IBOutlet weak var ballImageView: NSImageView!
+    @IBOutlet weak var nameTextField: NSTextField!
+    
+    @IBOutlet weak var showLabel: NSTextField!
+    @IBOutlet weak var sureButton: NSButton!
+    
+    
+    let adviceList = ["Yes",
+                      "NO",
+                      "Ray says 'do it!'",
+                      "Try again later",
+                      "How can I know?",
+                      "Totally",
+                      "Never",]
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
+        adviceTextField.hidden = true
+        ballImageView.image = NSImage(named: "8ball")
+           }
 
     override var representedObject: AnyObject? {
         didSet {
+            
         // Update the view, if already loaded.
         }
+       
+    }
+    
+    @IBAction func sureButtonClick(sender: NSButton) {
+          showLabel.stringValue = "hello \(nameTextField.stringValue)"
+        
+    }
+    @IBAction func tapGestureClickd(sender: AnyObject) {
+        if (adviceTextField.hidden) {
+            if let advice = adviceList.randomElement{
+                adviceTextField.stringValue = advice
+                adviceTextField.hidden = false
+                ballImageView.image = NSImage(named: "magic8ball")
+            }
+            
+        }else{
+            adviceTextField.hidden = true
+            ballImageView.image = NSImage(named: "8ball")
+            
+        }
+        
     }
 
-
 }
+// 给NSArry添加新属性
+extension Array{
+    var randomElement:Element?{
+        if count < 1 {
+            return .None
+        }
+        let randomIndex = arc4random_uniform(UInt32(count))
+        return self[Int(randomIndex)]
+    }
+}
+
 
