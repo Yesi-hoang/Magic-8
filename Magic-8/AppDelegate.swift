0105009e36eb9bf3fd9c14956dd9ae6e8451dae8
Yesi <317447721@qq.com>
2016-07-05 21:33:34 +0800

    Initial Commit

---
+++ Magic-8/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Magic-8
+//
+//  Created by 黄丽萍 on 16/7/5.
+//  Copyright © 2016年 黄丽萍. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Magic-8/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Magic-8
+//
+//  Created by 黄丽萍 on 16/7/5.
+//  Copyright © 2016年 黄丽萍. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
